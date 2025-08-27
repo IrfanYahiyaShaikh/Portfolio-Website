d00f78ed33a60e166caf0d39b7c8679556d1566c
IrfanShaikh <irfanyahiya711@gmail.com>
2025-08-27 23:33:44 +0530

    changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("index.html");
 });
 
 app.get("/about", (req, res) => {
