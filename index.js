b3fbeba555444a77632246b690e09d6eb1c90fa8
IrfanShaikh <irfanyahiya711@gmail.com>
2025-08-27 23:58:52 +0530

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-    res.render("index.html");
+    res.render("index.ejs");
 });
 
 app.get("/about", (req, res) => {
